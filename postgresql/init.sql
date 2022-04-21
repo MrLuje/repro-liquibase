5ae4d79d33c67cbaa0e5efd2b458daef58a83293
MrLuje <ogmniluje@gmail.com>
2022-04-21 19:34:44 +0200

    init

---
+++ postgresql/init.sql
@@ -0,0 +1,5 @@
+CREATE TABLE sample_table (
+    id INT NOT NULL,
+    created_date TIMESTAMP,
+    CONSTRAINT pk_id PRIMARY KEY (id)
+)
---
+++ postgresql_target/init.sql
@@ -0,0 +1,5 @@
+CREATE TABLE sample_table (
+    sample_table_id INT NOT NULL,
+    created_date TIMESTAMP,
+    CONSTRAINT pk_sample_table_id PRIMARY KEY (sample_table_id)
+)
